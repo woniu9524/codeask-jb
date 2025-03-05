bb207148b78deb4100c9eb126bed61658c1753a2
GitHub Action <action@github.com>
2025-03-05 02:30:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/woniu9524/codeaskjb/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/woniu9524/codeaskjb/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.woniu9524.codeaskjb.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.woniu9524.codeaskjb.MyBundle
+import com.github.woniu9524.codeaskjb.services.MyProjectService
 import javax.swing.JButton
 
 
